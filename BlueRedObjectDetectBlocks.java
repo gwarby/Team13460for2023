6dd45c667eb17f3924ee5a3a5af1985f8250d5f2
Nathan Fitzgerald <natefitz@gmail.com>
2023-12-19 09:53:52 -0600

    feat: this new class allows use of our OpenCV vision from Blocks - proof of concept

---
+++ BlueRedObjectDetectBlocks.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion;
+
+public class BlueRedObjectDetectBlocks extends BlocksOpModeCompanion {
+    static AutoCommon visLib = null;
+
+    @ExportToBlocks (
+        parameterLabels = {"colorCh, 1:red 2:blue"}
+        tooltip = "Use OpenCV Pipeline to scan for the location of a 'very' blue or red block",
+    )
+    public static void InitPropDetection(int colorChannel) {
+        FindPropVisInitData visInitData = new FindPropVisInitData();
+        visInitData.ColorChannel = colorChannel;
+        visLib = new AutoCommon(
+            hardwareMap,
+            new IFtcOpmodeSleep() { @Override public void FtcSleepFnc(int ms) { sleep(ms); } },
+            visInitData);
+    }
+
+    @ExportToBlocks 
+    (
+        comment = "By disabling pipeline, CPU usage can be potentially reduced",
+    )
+    public static void DisablePropDetection() {
+        if (visLib == null)
+            return;
+
+        visLib.FindPropSetEnableDetection(false);
+    }
+
+    @ExportToBlocks (
+        parameterLabels = {"ColorChannel"}
+        comment = "Returns LEFT, MIDDLE or RIGHT String values",
+        tooltip = "Returns LEFT, MIDDLE or RIGHT String values"
+    )
+    public static String PropLocation () {
+        if (visLib != null) {
+            return visLib.FindPropLocation();
+        }
+        return "ERROR: Prop detection not initialized or otherwise null object";
+    }
+}
